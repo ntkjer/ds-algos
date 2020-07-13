a5280a26414374830d937c9ae12f0de5cf038d61
ntkjer <ntkjer@gmail.com>
2020-07-13 19:07:41 -0400

    added qsort and 3 way part

--- sorting/quick.py
+++ sorting/quick.py
@@ -1,55 +1,60 @@
 import math
 
 def qsort(my_list):
-    quick_sort(my_list, 0, len(my_list)-1)
+    n = len(my_list) - 1
+    return quick_sort(my_list, 0, n), quick_sort_3(my_list, 0, n)
 
 def quick_sort(my_list, lo, hi):
-    if lo < hi:
-        k = partition_a(my_list, lo, hi)
-        quick_sort(my_list, lo, k - 1)
-        quick_sort(my_list, k + 1, hi)
-
+    if lo >= hi: return
+    k = partition(my_list, lo, hi)
+    quick_sort(my_list, lo, k - 1)
+    quick_sort(my_list, k + 1, hi)
+    return my_list
 
 def partition(my_list, lo, hi):
-    pivot = my_list[hi + lo // 2]
-    i = lo
-    j = hi + 1
-    while True:
-        while my_list(i) < pivot:
-            if i == hi:
-                break
-#        while pivot < my_list[j]
-
-def partition_a(my_list, lo, hi):
     i = lo-1
     pivot = my_list[hi]
     for j in range(lo, hi):
         if my_list[j] <= pivot:
             i = i + 1
-            my_list[i], my_list[j] = my_list[j], my_list[i],
+            my_list[i], my_list[j] = my_list[j], my_list[i]
 
     my_list[i+1], my_list[hi] = my_list[hi], my_list[i+1]
     return i + 1
 
-def lomuto_partition(my_list, lo, hi):
-    pivot = my_list[hi]
-    j = i = lo
-    while j < hi:
-        if my_list[j] < pivot:
-            swap = my_list[i]
-            my_list[i] = my_list[j]
-            my_list[j] = swap
+def quick_sort_3(my_list, lo, hi):
+    if hi <= lo: return
+    lt, i, gt = lo, lo+1, hi
+    v = my_list[lo]
+    while i <= gt:
+        comparison = compare(my_list[i], v)
+        if comparison < 0: 
+            swap(my_list, lt, i)
+            i += 1
+        elif comparison > 0:
+            swap(my_list, i, gt)
+            gt -= 1
+        else:
             i += 1
 
-    swap = my_list[i]
-    my_list[i] = my_list[hi]
-    my_list[hi] = swap
-    return i
+    quick_sort_3(my_list, lo, lt -1)
+    quick_sort_3(my_list, gt + 1, hi)
+    return my_list
 
 
+def swap(my_list, j, k):
+    my_list[j], my_list[k] = my_list[k], my_list[j]
 
-    return
+def compare(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    else:
+        return -1
 
 if __name__ == '__main__':
     l = [-3, -4, -5, -6, 0, 10, -199]
-    print(qsort(l))
+    a, b = qsort(l)
+    print("quick sort: ", a)
+    print("quick sort, 3 way part: ", b)
